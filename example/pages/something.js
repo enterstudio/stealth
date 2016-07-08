8e31eefd182a519f1a2642a9eba9f9812aa99c2c
Oliver Rumbelow <oliver.rumbelow@holidayextras.com>
2016-07-08 19:07:55 +0100

    stuff

--- example/pages/something.js
+++ example/pages/something.js
@@ -3,6 +3,7 @@ module.exports = {
   template: "/something",
   preRender: function(request, callback) {
     setTimeout(function() {
+      throw new Error();
       return callback(null, { i: request.session.i || 0 });
     }, 2000);
   }
