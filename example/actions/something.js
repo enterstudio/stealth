5d869a35f3677419858b3a0f140eec19946cf300
Oliver Rumbelow <oliver.rumbelow@holidayextras.com>
2016-07-09 19:22:21 +0100

    stuff

--- example/actions/something.js
+++ example/actions/something.js
@@ -3,6 +3,7 @@ module.exports = {
   redirect: "/something",
   action: function(request, callback) {
     request.session.i++;
+    // throw new Error();
     return callback(null, { foo: "bar" });
   }
 };
--- example/pages/something.js
+++ example/pages/something.js
@@ -3,7 +3,7 @@ module.exports = {
   template: "/something",
   preRender: function(request, callback) {
     setTimeout(function() {
-      throw new Error();
+      // throw new Error();
       return callback(null, { i: request.session.i || 0 });
     }, 2000);
   }
