e87db2ad640c524bec5d82c634b7c60c8783aac3
Oliver Rumbelow <oliver.rumbelow@holidayextras.com>
2016-09-11 18:39:52 +0100

    stuff

--- bin/stealth.js
+++ bin/stealth.js
@@ -2,11 +2,15 @@
 var argv = require('minimist')(process.argv.slice(2));
 var shell = require('shelljs');
 
+if (argv._.indexOf('init') !== -1) {
+  shell.mkdir('-p', [ 'actions', 'config', 'pages', 'static', 'templates']);
+  shell.cp(__dirname + '/../build/*', '.');
+}
+
 if (argv._.indexOf('start') !== -1) {
   require('stealth').start();
 }
 
-if (argv._.indexOf('init') !== -1) {
-  shell.mkdir([ 'actions', 'config', 'pages', 'static', 'templates']);
-  console.log("I'm here", __dirname)
+if (argv._.indexOf('build') !== -1) {
+  shell.exec('packer build ./packer.json ');
 }
